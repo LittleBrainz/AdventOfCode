e697a0faeeb678675b335d99d4e4a380a55ea4a7
Stephen Turner <stephen@littlebrainz.com>
2024-12-25 20:01:28 +0000

    2024 Day 01 use dataclass

--- 2024/day01/solution.py
+++ 2024/day01/solution.py
@@ -6,6 +6,13 @@
 
 
 from collections import Counter
+from dataclasses import dataclass
+
+
+@dataclass
+class Data:
+    l_list: list[int]
+    r_list: list[int]
 
 
 def main(path: str) -> None:
@@ -16,23 +23,21 @@ def main(path: str) -> None:
     print(f'\n{path}:\n  Part 1 = {part_1}\n  Part 2 = {part_2}')
 
 
-def parse_text(text: str) -> tuple[list[int], list[int]]:
+def parse_text(text: str) -> Data:
     lines = text.split('\n')
     l_list = [int(line.split('   ')[0]) for line in lines]
     r_list = [int(line.split('   ')[1]) for line in lines]
-    return (l_list, r_list)
+    return Data(l_list, r_list)
 
 
-def calc_part_1(data: tuple[list[int], list[int]]) -> int:
-    (l_list, r_list) = data
-    d_list = [abs(l - r) for l, r in zip(sorted(l_list), sorted(r_list))]
+def calc_part_1(data: Data) -> int:
+    d_list = [abs(l - r) for l, r in zip(sorted(data.l_list), sorted(data.r_list))]
     return sum(d_list)
 
 
-def calc_part_2(data: tuple[list[int], list[int]]) -> int:
-    (l_list, r_list) = data
-    r_dict = Counter(r_list)
-    c_list = [l * r_dict.get(l, 0) for l in l_list]
+def calc_part_2(data: Data) -> int:
+    r_dict = Counter(data.r_list)
+    c_list = [l * r_dict.get(l, 0) for l in data.l_list]
     return sum(c_list)
 
 
