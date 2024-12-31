723e0dcd4a29bdc00f313a846bb682d7caed3826
Stephen Turner <stephen@littlebrainz.com>
2024-12-31 21:53:37 +0000

    2024 Day 04 complete

--- 2024/day04/solution.py
+++ 2024/day04/solution.py
@@ -11,8 +11,8 @@ from dataclasses import dataclass
 @dataclass
 class Data:
     lines: list[str]
-    xm: int
     ym: int
+    xm: int
 
 
 def main(path: str) -> None:
@@ -25,29 +25,37 @@ def main(path: str) -> None:
 
 def parse_text(text: str) -> str:
     lines = [line for line in text.split('\n')]
-    return Data(lines, len(lines[0])-1, len(lines)-1)
+    return Data(lines, len(lines) - 1, len(lines[0]) - 1)
 
 
-def calc_part_1(data: Data) -> int:
-    munges = [munge(data, xd, yd) for (xd, yd) in
-        [(1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1), (0,-1), (1,-1)]]
-    return 0
+def calc_part_1(d: Data) -> int:
+    l_r = [[d.lines[y][x]
+            for y in rrange(0, d.ym)]
+                for x in rrange(0, d.xm)]
+    t_b = [[d.lines[y][x]
+            for x in rrange(0, d.xm)]
+                for y in rrange(0, d.ym)]
+    dup = [[d.lines[y-x][x]
+            for x in rrange(max(0, y - d.ym), min(d.xm, y))]
+                for y in rrange(0, d.ym + d.xm)]
+    ddn = [[d.lines[y+x][x]
+            for x in rrange(max(0, -y), min(d.xm, d.ym - y))]
+                for y in rrange(-d.xm, d.ym)]
+    slices = ' '.join(''.join(cs) for cs in l_r + t_b + dup + ddn)
+    return (slices + ' ' + slices[::-1]).count('XMAS')
 
 
-def calc_part_2(data: Data) -> int:
-    return 0
-
-
-def munge(data: Data, xd: int, yd: int) -> str:
-    mag = data.rows + data.cols
-    for m in range(-mag, mag):
-        pass
-    text = ''
-    for row in range(data.rows):
-        for col in range(data.cols):
-            text += data.lines[row][col]
-        text += ' '
-    return text
+def calc_part_2(d: Data) -> int:
+    count = 0
+    for y in rrange(1, d.ym - 1):
+        for x in rrange(1, d.xm - 1):
+            if d.lines[y][x] != 'A':
+                continue
+            msms = d.lines[y-1][x-1] + d.lines[y+1][x+1] + d.lines[y-1][x+1] + d.lines[y+1][x-1]
+            if msms not in 'MSMS MSSM SMMS SMSM':
+                continue
+            count += 1
+    return count
 
 
 def rrange(i0: int, i1: int) -> range:
@@ -58,4 +66,4 @@ def rrange(i0: int, i1: int) -> range:
 
 if __name__ == '__main__':
     main('example.txt')
-    # main('input.txt')
+    main('input.txt')
